05adf6a49db2fc47972dfbc6c3222fe0cf7ba060
Thong Dang <danghoangthong274@gmail.com>
2018-09-01 01:07:13 -0500

    Modified the code to print out message and name. Add some notes in the comment sections.

--- logger.js
+++ logger.js
@@ -1,11 +1,11 @@
 
-// var url ='http://mylogger.io/log';
+// This module contain the function log(message) that will print out the message on the console.
 
-function log(message) {
+function log(message, name) {
     // print a message on console.
-    console.log(message);
+    console.log(message + " " + name + "!");
 
 }
 
+// exported_log is the name of the function log that will be called outside this modules.
 module.exports.exported_log = log;
-// module.export.endPoint_url = url;
---
+++ message_name.js
@@ -0,0 +1,5 @@
+// Use this to load the module. 
+// Best practice: Use const when load a console.
+const logger = require('./logger');
+
+logger.exported_log('Hello', 'Thong');
