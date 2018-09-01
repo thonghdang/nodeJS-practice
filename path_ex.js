dc2de225da67b083a0171d91d3b816157bc9c141
Thong Dang <danghoangthong274@gmail.com>
2018-09-01 12:47:14 -0500

    Modified path_ex.js.

--- path_ex.js
+++ path_ex.js
@@ -1,16 +1,3 @@
-// Working with os module.
-
-const os = require('os');
-
-var totalMemory = os.totalmem();
-var freeMemory = os.freemem();
-
-console.log('Total Memory: ' + totalMemory + '\n');
-
-// Template String of ES6
-console.log(`Using Template String of ES6: `);
-console.log(`Total Memory: ${totalMemory}`);
-console.log(`Free Memory: ${freeMemory}`);
 // Example of using path module.
 
 const path = require('path');
