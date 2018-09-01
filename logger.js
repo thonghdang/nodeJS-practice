5a060a6e45079256451acba90b249ca17a654a9d
Thong Dang <danghoangthong274@gmail.com>
2018-09-01 00:41:22 -0500

    Add the logger modules.

---
+++ logger.js
@@ -0,0 +1,11 @@
+
+// var url ='http://mylogger.io/log';
+
+function log(message) {
+    // print a message on console.
+    console.log(message);
+
+}
+
+module.exports.exported_log = log;
+// module.export.endPoint_url = url;
