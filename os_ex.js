cb83cf3e1a76e99dab58ae19955d5dc977bc8a0b
Thong Dang <danghoangthong274@gmail.com>
2018-09-01 12:46:04 -0500

    Added comment in path_ex.js. Created os_ex.js to print out memory usage on the console.

--- os_ex.js
+++ os_ex.js
@@ -5,16 +5,9 @@ const os = require('os');
 var totalMemory = os.totalmem();
 var freeMemory = os.freemem();
 
-console.log('Total Memory: ' + totalMemory + '\n');
+console.log('Total Memory: ' + totalMemory);
 
 // Template String of ES6
 console.log(`Using Template String of ES6: `);
 console.log(`Total Memory: ${totalMemory}`);
 console.log(`Free Memory: ${freeMemory}`);
-// Example of using path module.
-
-const path = require('path');
-
-var pathObj = path.parse(__filename);
-
-console.log(pathObj);
