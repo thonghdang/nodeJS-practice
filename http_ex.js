482eb097e099f3489449173e9efc4fe82361fcbc
Thong Dang <danghoangthong274@gmail.com>
2018-09-01 20:42:46 -0500

    Modified the http_ex.js.

--- http_ex.js
+++ http_ex.js
@@ -2,11 +2,23 @@
 
 const http = require('http');
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+    if (req.url == '/')
+    {
+        res.write('Hello World');
+        res.end();
+    }
 
-server.on('connection', (socket) => {
-   console.log('New connection'); 
-})
+    if (req.url == '/api/courses')
+    {
+        res.write(JSON.stringify([1, 2, 3]));
+        res.end();
+    }
+});
+
+// server.on('connection', (socket) => {
+//    console.log('New connection'); 
+// })
 
 server.listen(3000);
 
