0bcfaa14d4997190d9b66790a72bd671a3d324e6
Thong Dang <danghoangthong274@gmail.com>
2018-09-10 23:27:54 -0500

    Modify index.js. Add exception handling when the id is not found

--- express-demo/index.js
+++ express-demo/index.js
@@ -2,16 +2,24 @@
 const express = require('express');
 const app = express();
 
+const courses = [
+    {id: 1, name: 'course1'},
+    {id: 2, name: 'course2'},
+    {id: 3, name: 'course3'},
+];
+
 app.get('/', (req, res) => {
     res.send('Hello, World!!!');
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1,2,3]);
+    res.send(courses);
 });
 
-app.get('/api/posts/:year/:month', (req, res) => {
-    res.send(req.params);
+app.get('/api/courses/:id', (req, res) => {
+   let course = courses.find(c => c.id === parseInt(req.params.id));
+   if (!course) res.status(404).send('The course with the given ID was not found.');
+   res.send(course);
 });
 
 // PORT
